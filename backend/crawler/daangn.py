e38aa87d9efb8b586d97aba75b509ab31738fdee
Jungwoo Lee <lmwljw98@gmail.com>
2022-03-18 16:16:11 +0900

    fix: 포맷팅 적용

--- backend/crawler/daangn.py
+++ backend/crawler/daangn.py
@@ -1,9 +1,11 @@
 import requests
 
-def get_dangn(keyword, pages):
-  for page in range(1, pages+1):
-    url = f'https://www.daangn.com/search/{keyword}/more/flea_market?page={page}'
-    res = requests.get(url)
-    print(res.text)
 
-#get_dangn("맥북", 3)
+def get_dangn(keyword, pages):
+    for page in range(1, pages + 1):
+        url = f"https://www.daangn.com/search/{keyword}/more/flea_market?page={page}"
+        res = requests.get(url)
+        print(res.text)
+
+
+# get_dangn("맥북", 3)
