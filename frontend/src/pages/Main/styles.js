8a7b4f3af96756b03702e642c38780615a2c2d78
asegh12 <lgh009833@kookmin.ac.kr>
2022-04-03 05:23:04 +0900

    fix: border 수정

--- frontend/src/pages/Main/styles.js
+++ frontend/src/pages/Main/styles.js
@@ -15,19 +15,18 @@ export const ProductItem = styled(NavLink)`
   display: flex;
   flex-direction: column;
   align-items: center;
-  border: 0.1rem solid rgb(228, 228, 228);
   border-radius: 1rem;
   padding: 0.8rem;
   cursor: pointer;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.15);
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.2);
   transition: all 0.5s;
   text-decoration: none;
   color: black;
   font-weight: 500;
 
   &:hover {
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
-    -webkit-transform:scale(1.1); 
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    -webkit-transform: scale(1.1); 
   }
 `;
 
