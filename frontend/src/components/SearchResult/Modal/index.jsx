f1928461bb2bf3f9e69c4e8cc6c7b46776f58764
asegh12 <lgh009833@kookmin.ac.kr>
2022-05-13 19:38:40 +0900

    fix: modal 수정

--- frontend/src/components/SearchResult/Modal/index.jsx
+++ frontend/src/components/SearchResult/Modal/index.jsx
@@ -7,15 +7,9 @@ function Modal({ detail, onClose }) {
 
   useEffect(() => {
     console.log(detail);
-    document.body.style.cssText = `
-      position: fixed; 
-      top: -${window.scrollY}px;
-      overflow-y: scroll;
-      width: 100%;`;
+    document.body.style.overflow = 'hidden';
     return () => {
-      const scrollY = document.body.style.top;
-      document.body.style.cssText = '';
-      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+      document.body.style.overflow = 'visible';
     };
   }, []);
 
