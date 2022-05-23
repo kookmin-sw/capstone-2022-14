a39009c658bcf037d57287ecc08363656ced0288
asegh12 <lgh009833@kookmin.ac.kr>
2022-05-23 15:21:45 +0900

    fix: 제품 버튼 디자인 수정

--- frontend/src/components/ProductList/styles.js
+++ frontend/src/components/ProductList/styles.js
@@ -25,13 +25,13 @@ export const ItemWrapper = styled.div`
 
 export const ItemBtn = styled.button`
   margin: 1rem 2rem;
-  font-size: 1rem;
+  font-size: 0.9rem;
   border: 0;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.4);
   padding: 1rem 2rem;
   background-color: ${props =>
-    props.isClicked ? 'rgba(0, 0, 0, 0.2)' : 'white'};
-  font-weight: ${props => (props.isClicked ? 'bold' : '400')};
+    props.isClicked ? 'rgba(0, 0, 0, 0.4)' : 'white'};
+  color: ${props => (props.isClicked ? 'white' : 'black')};
   border-radius: 1rem;
   cursor: pointer;
   transition: all 0.5s;
